e10e6879f33cee5b1500cea1d1062d259da69bdd
Fletcher Morris <odmoandtheboy@gmail.com>
2018-11-28 03:56:50 +0000

    Added classes for Objects and Vector2

--- client.js
+++ client.js
@@ -1,7 +1,38 @@
 var canvas;
+var context;
 
 window.addEventListener("load", function()
 {
     canvas = document.getElementById('MainCanvas');
+    context = canvas.getContext("2d");
 
 }, false);
+
+
+var renderQueue = [];
+
+function Render()
+{
+    context.clearRect(0,0,800,600);
+}
+
+
+
+
+class Vector2
+{
+    constructor(x,y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+}
+class Object
+{
+    constructor(name, position)
+    {
+        this.name = name;
+        this.position = position;
+        this.renderable = false;
+    }
+}
