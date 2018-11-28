efc1082dcc60580d86e39eac245a08769ecfd7d6
Fletcher Morris <odmoandtheboy@gmail.com>
2018-11-28 21:36:20 +0000

    minor changes

--- client.js
+++ client.js
@@ -1,23 +1,29 @@
 var canvas;
 var context;
 
+var img = new Image();
+img.src = 'images/test_img.png';
+var testImage
+
 window.addEventListener("load", function()
 {
     canvas = document.getElementById('MainCanvas');
-    context = canvas.getContext("2d");
+    context = canvas.getContext('2d');
+    
+    
+    testImage = new Renderable("ralsei", new Vector2(50,50), img);
+
+    Update();
 
 }, false);
 
-
-var renderQueue = [];
-
-function Render()
+function Update()
 {
     context.clearRect(0,0,800,600);
+    context.font = "30px Arial";
+    context.fillText("Hello World", 10, 50);
+    testImage.Update();
 }
-
-
-
 
 class Vector2
 {
@@ -29,10 +35,36 @@ class Vector2
 }
 class Object
 {
-    constructor(name, position)
+    constructor(name, pos)
     {
         this.name = name;
-        this.position = position;
+        this.pos = pos;
         this.renderable = false;
+    }
+
+    Update()
+    {
+
+    }
+}
+class Renderable extends Object
+{
+    constructor(name, pos, image)
+    {
+        super(name, pos);
+        this.image = image;
+    }
+
+    Update()
+    {
+        super.Update();
+
+        this.Render();
+    }
+
+    Render()
+    {
+        context.drawImage(this.image, this.pos.x, this.pos.y);
+        console.log("Rendered Ralsei");
     }
 }
